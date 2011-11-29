365c3fc00d34e84db399a415d8051eb61d50a474
Marius Mundal <mm@mundal.org>
2011-11-29 21:38:54 +0100

    Created initial Response object Started CHSysInfo handler implementation

--- MySocketServer/src/com/scada/server/handlers/CHSysInfo.java
+++ MySocketServer/src/com/scada/server/handlers/CHSysInfo.java
@@ -1,5 +1,12 @@
 package com.scada.server.handlers;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import com.scada.utils.Command;
 import com.scada.utils.ProtocolUtils;
 
@@ -14,6 +21,43 @@ public class CHSysInfo extends HandlerBase {
 		if(!c.getCommandType().equals(super.getCommandType())) {
 			System.out.println("Command Type missmatch in CommandHandler " + super.getCommandType() + ". Command not handled");
 		}
+		else {
+			File directory = new File("D:\\DEV\\DUMPDIR\\");
+			File files[] = directory.listFiles();
+			for (File f : files) {   
+				parseFile(f);
+			}	
+		}
+	}
+	
+	private void parseFile(File file) {
+		FileInputStream fis = null;
+	    BufferedInputStream bis = null;
+	    DataInputStream dis = null;
+	 
+	    try {
+	       fis = new FileInputStream(file);
+	       bis = new BufferedInputStream(fis);
+	       dis = new DataInputStream(bis);
+	 
+	       while (dis.available() != 0) {
+	    	   System.out.println(dis.readLine());
+	       }
+	    } catch (FileNotFoundException e) {
+	       e.printStackTrace();
+	    } catch (IOException e) {
+	       e.printStackTrace();
+	    } finally {
+	    	try {
+	    		fis.close();
+	    		bis.close();
+	    		dis.close();
+	    	} catch (IOException e) {
+	 	       fis = null;
+	 	       bis = null;
+	 	       dis = null;
+		    }
+	    }
 	}
 	
 	
--- MySocketServer/src/com/scada/utils/Response.java
+++ MySocketServer/src/com/scada/utils/Response.java
@@ -2,10 +2,17 @@ package com.scada.utils;
 
 public class Response {
 	private String responseType;
+	private String key;
+	private String value;
 	
 	public Response(String type) {
 		responseType = type;
 	}
 	
 	public String getCommandType() { return responseType; }
+	
+	public String getKey() { return key; }
+	public void setKey( String key) { this.key = key; }
+	public String getValue() { return value; }
+	public void setValue( String value) { this.value = value; }
 }
